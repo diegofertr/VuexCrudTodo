49bbb7c66f0f5750ee616a8909bc959bf42a9877
diegofertr <diegonano09@gmail.com>
2019-01-03 01:54:24 -0400

    iniciando componente TodoList para listado de tareas

---
+++ src/components/TodoList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div v-if="todos.length">
+    
+  </div>
+  <b-alert 
+    v-else
+    show
+    variant="info">No existen TODOS</b-alert>
+</template>
+
+<script>
+export default {
+  props: {
+    todos: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
