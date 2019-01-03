6e365b712b09e8c298365397f92f31cf42b92c54
diegofertr <diegonano09@gmail.com>
2019-01-03 02:03:14 -0400

    Avances componente TodoItem

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+import {mapMutations, mapActions} from 'vuex'
+export default {
+  props: {
+    todo: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    ...mapActions({
+      _updateTodoStatus: 'todos/updateTodoStatus',
+      _removeTodo: 'todos/removeTodo'
+    }),
+    ...mapMutations('todos', ['setTodo']),
+    goToUpdateTodo (todo) {
+      this.setTodo(todo)
+      this.$router.push({
+        name: 'todos-update',
+        params: {
+          id: todo.id
+        }
+      })
+    },
+    updateTodoStatus (todo) {
+      this._updateTodoStatus(todo)
+    },
+    removeTodo (id) {
+      this._removeTodo(id)
+    }
+  }
+}
+</script>
+
